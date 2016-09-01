6ff3fa688a427327b54c1d9ca8bfbb552a9fb36b
RonaldM- <ronald.marcel.mr@gmail.com>
2016-09-01 17:30:09 +0200

    exo 1

--- exemple/Exercice 1/exo_1.php
+++ exemple/Exercice 1/exo_1.php
@@ -1,23 +1,118 @@
-<form action="panier.php" method="post">
+<style>
+    .box_list{
+        float: left;
+        margin-left: 30px;
+    }
+</style>
+    
+
+<form action="panier.php" method="get">
     <h1 class="title">Panier d'achat</h1>
+<!--    <div class="box_list">-->
     <p>Item 1#</p>
     <!--<input type="text" name="name">-->
     <p>Quantité</p>
-    <select name="quantity">
-        <option value="">1</option>
-        <option value="">2</option>
-        <option value="">3</option>
-        <option value="">4</option>
-        <option value="">5</option>
-        <option value="">6</option>
-        <option value="">7</option>
-        <option value="">8</option>
-        <option value="">9</option>
-        <option value="">10</option>
+    <select name="quantity_item1">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>    
     </select>
     <p>Price</p>
-    <input type="text" name="price" size="4">€<br>
-    <pTva<br>
+    <input type="text" name="price_item1" size="4">€<br>
+
     <input type="text" name="tva" size="2">%<br>
+<!--    </div>-->
+    
+    <div class="box_list">
+    <p>Item 2#</p>
+    <!--<input type="text" name="name">-->
+    <p>Quantité</p>
+    <select name="quantity_item2">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>   
+    </select>
+    <p>Price</p>
+    <input type="text" name="price_item2" size="4">€<br>
+
+    <input type="text" name="tva" size="2">%<br>
+    </div>
+    <div class="box_list">
+    <p>Item 3#</p>
+    <!--<input type="text" name="name">-->
+    <p>Quantité</p>
+    <select name="quantity_item3">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>   
+    </select>
+    <p>Price</p>
+    <input type="text" name="price_item3" size="4">€<br>
+
+    <input type="text" name="tva" size="2">%<br>
+    </div>
+    <div class="box_list">
+    <p>Item 4#</p>
+    <!--<input type="text" name="name">-->
+    <p>Quantité</p>
+    <select name="quantity_item4">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>   
+    </select>
+    <p>Price</p>
+    <input type="text" name="price_item4" size="4">€<br>
+
+    <input type="text" name="tva" size="2">%<br>
+    </div>
+    <div class="box_list">
+    <p>Item 5#</p>
+    <p>Quantité</p>
+    <select name="quantity_item5">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>   
+    </select>
+    <p>Price</p>
+    <input type="text" name="price_item5" size="4">€<br>
+
+    <input type="text" name="tva" size="2">%<br>
+    </div>
+    <br>
     <input type="submit" value="Value">
 </form>
